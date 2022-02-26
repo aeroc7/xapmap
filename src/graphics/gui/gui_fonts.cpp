d3e0d93be1ebe0a71934762aa51a71d061f42acd
Bennett <aerozotac@outlook.com>
2022-02-25 20:43:31 -0800

    Include font binary C arrays in build system

---
+++ src/graphics/gui/gui_fonts.cpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2022 Bennett Anderson
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <Roboto_Black.cpp>
+#include <Roboto_BlackItalic.cpp>
+#include <Roboto_Bold.cpp>
+#include <Roboto_Italic.cpp>
+#include <Roboto_Light.cpp>
+#include <Roboto_LightItalic.cpp>
+#include <Roboto_Medium.cpp>
+#include <Roboto_MediumItalic.cpp>
+#include <Roboto_Regular.cpp>
+#include <Roboto_Thin.cpp>
+#include <Roboto_ThinItalic.cpp>
